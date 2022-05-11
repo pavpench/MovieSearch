f4319597168ecbaefbf3da12f72034509ded6134
p.penchev <p.penchev90@gmail.com>
2022-05-11 20:07:00 +0200

    FE/BE_communication-ready; 3rd party Api_data available

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const url = require("url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -10,24 +11,32 @@ const server = http.createServer(async (req, res) => {
     "Access-Control-Allow-Max-Age": 259200,
     "Content-Type": "application/json",
   };
+  //access request.params data
+  let requestParamsObject = url.parse(req.url, true).query;
 
-  //access request data
-  let bufferData = [];
+  let requestParams =
+    (requestParamsObject.s ? `&s=${requestParamsObject.s}` : "") +
+    (requestParamsObject.y ? `&y=${requestParamsObject.y}` : "");
 
-  for await (const chunk of req) {
-    bufferData.push(chunk);
-  }
-  let searchQuery = bufferData.toString();
+  console.log(requestParams);
+
+  //access request.body data
+  // let bufferData = [];
+  // for await (const chunk of req) {
+  //   bufferData.push(chunk);
+  // }
+  // let requestBody = bufferData.toString();
 
   let apiRequestQuery =
-    `http://www.omdbapi.com/?apikey=5945647d&` + searchQuery;
-  console.log(searchQuery);
+    `http://www.omdbapi.com/?apikey=5945647d` + requestParams;
 
   http.get(apiRequestQuery, (resp) => {
+    console.log("request sent to api response:", resp);
     if (resp.statusCode === 200) {
       res.writeHead(200, headers);
       resp
         .on("data", (data) => {
+          console.log(data.toString());
           res.write(data.toString());
         })
         .on("error", (err) => {
