8018f32335f9e93153c3bfa83c235bd1f1b20491
p.penchev <p.penchev90@gmail.com>
2022-05-11 20:13:01 +0200

    basic user input interface-Search variables

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,39 +1,14 @@
-import React, { useState, useContext } from "react";
-import { getData } from "../api";
-import { StorageContext } from "../contexts";
-import {
-  loadingAction,
-  doneAction,
-  errorAction,
-  noDataAction,
-} from "../actions";
+import React, { useState } from "react";
+import { searchBtnClick } from "../api";
 
 export default function Search() {
   const [title, setTitle] = useState("");
   const [year, setYear] = useState("");
   const [type, setType] = useState("");
-  const mainState = useContext(StorageContext);
 
-  const searchBtnClick = () => {
-    // mainState.dispatch(loadingAction());
+  //Confirm search
+  searchBtnClick(title, year, type);
 
-    getData(title, year, type)
-      .then((data) => {
-        // console.log(title);
-        // console.log("data", data);
-        if (data) {
-          mainState.dispatch(doneAction(data));
-        } else {
-          mainState.dispatch(noDataAction());
-        }
-
-        console.log("get data triggered", data);
-      })
-      .catch((error) => {
-        mainState.dispatch(errorAction());
-        console.log(error);
-      });
-  };
   return (
     <div className="row">
       <div className="col-12">
@@ -61,15 +36,15 @@ export default function Search() {
       <div className="col-12">
         <div className="input-group mb-3 mt-3">
           <div className="input-group-prepend">
-            <label className="input-group-text">Type</label>
+            <label className="input-group-text">Year</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Movie title or keyword"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
           </div>
-          <select
-            className="form-control"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}>
-            <option value="">Choose...</option>
-            <option value="grayscale">grayscale</option>
-          </select>
         </div>
       </div>
       <div className="col-12">
@@ -82,30 +57,12 @@ export default function Search() {
             value={type}
             onChange={(e) => setType(e.target.value)}>
             <option value="">Choose...</option>
-            <option value="backgrounds">backgrounds</option>
-            <option value="fashion">fashion</option>
-            <option value="nature">nature</option>
-            <option value="science">science</option>
-            <option value="education">education</option>
-            <option value="feelings">feelings</option>
-            <option value="health">health</option>
-            <option value="people">people</option>
-            <option value="religion">religion</option>
-            <option value="places">places</option>
-            <option value="animals">animals</option>
-            <option value="industry">industry</option>
-            <option value="computer">computer</option>
-            <option value="food">food</option>
-            <option value="sports">sports</option>
-            <option value="transportation">transportation</option>
-            <option value="travel">travel</option>
-            <option value="buildings">buildings</option>
-            <option value="business">business</option>
-            <option value="music">music</option>
+            <option value="movies">movies</option>
+            <option value="series">series</option>
+            <option value="episode">episode</option>
           </select>
         </div>
       </div>
-      console.log(title, year);
     </div>
   );
 }
